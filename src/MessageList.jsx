5f13f0301f6ef1f924b421baa7d92272bfa9fc76
Fei Gao <fgao1@ualberta.ca>
2018-10-01 19:23:18 -0600

    create react components

---
+++ src/MessageList.jsx
@@ -0,0 +1,18 @@
+// (props) => {
+//     return (
+//         {props.messages.map(message => <Message />)}
+//     )
+// }
+import React, {Component} from 'react';
+import Message from './Message.jsx';
+
+class MessageList extends Component {
+  render() {
+    return (
+        <div className="messages">
+        <Message />
+        </div>
+    )
+  }
+}
+export default MessageList;
