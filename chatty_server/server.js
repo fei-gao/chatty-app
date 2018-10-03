d7d0917c4aba63cd2b77ee8a7d89847812248ba4
Fei Gao <fgao1@ualberta.ca>
2018-10-03 15:39:31 -0600

    show a notification when user changes name

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -20,26 +20,56 @@ const wss = new SocketServer({ server });
 const msgConstructor = function (msg){
     const randomId = uuidv1();
     const newMsg = {
+        type: 'incomingMessage',
         id: randomId,
         username: msg.username,
         content: msg.content
     }
     return newMsg;
 }
+
+const notificationConstructor = function(notification){
+    const newNotification = {
+        type: 'incomingNotification',
+        id: uuidv1(),
+        content: notification.content
+    }
+    return newNotification;
+}
+
+// const clients = [];
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
 wss.on('connection', (ws) => {
   console.log('Client connected');
-
-  ws.on('message', function incoming(message){
-      let msgObj = msgConstructor(JSON.parse(message));
-      wss.clients.forEach(function each(client) {
-          if (client.readyState === WebSocket.OPEN) {
-              client.send(JSON.stringify(msgObj));
-            }
-        });
-      console.log('=======received: %s', message, msgObj,JSON.stringify(msgObj));
+  // clients.push(ws);
+  ws.on('message', function incoming(data){
+      if(JSON.parse(data).type === 'postNotification'){
+        console.log("================", JSON.parse(data)); 
+        let notificationObj = notificationConstructor(JSON.parse(data));
+        wss.clients.forEach((client) => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify(notificationObj));
+              }
+          });
+      } else if(JSON.parse(data).type === 'postMessage'){
+          console.log('=======', JSON.parse(data));
+        let msgObj = msgConstructor(JSON.parse(data));
+        
+          wss.clients.forEach((client) => {
+              if (client.readyState === WebSocket.OPEN) {
+                  client.send(JSON.stringify(msgObj));
+                }
+            });
+      }
+    //   let msgObj = msgConstructor(JSON.parse(message));
+    //   wss.clients.forEach((client) => {
+    //       if (client.readyState === WebSocket.OPEN) {
+    //           client.send(JSON.stringify(msgObj));
+    //         }
+    //     });
+    //   console.log('=======received: %s', message, msgObj,JSON.stringify(msgObj));
   });
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
